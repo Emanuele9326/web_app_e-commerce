c8b6d632672dd4e2bde6e9599b71df71c2defcb3
EMANUELE <emanuelemarra93@outlook.com>
2022-09-09 12:09:20 +0200

    Update ProductCategorySeeder.php
    app+api

--- API/database/seeders/ProductCategorySeeder.php
+++ API/database/seeders/ProductCategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\ProductCategory;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,10 +18,22 @@ class ProductCategorySeeder extends Seeder
     {
         ProductCategory::insert(
             [
-                ['id'=> 1,'category'=>'pasta','unit_of_measure'=>'gr'],
-                ['id'=> 2,'category'=>'vini','unit_of_measure'=>'litri'],
-                ['id'=> 3,'category'=>'confetture','unit_of_measure'=>'gr'],
-                ['id'=> 4,'category'=>'biscotti','unit_of_measure'=>'gr'],
+                [
+                    'id' => 1, 'category' => 'pasta', 'unit_of_measure' => 'gr', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+                [
+                    'id' => 2, 'category' => 'vini', 'unit_of_measure' => 'litri', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+                [
+                    'id' => 3, 'category' => 'confetture', 'unit_of_measure' => 'gr', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+                [
+                    'id' => 4, 'category' => 'biscotti', 'unit_of_measure' => 'gr', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
             ]
             );
     }
